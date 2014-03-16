7d0da29a927b7146c240c57c5ab6553377c95fb9
Anthony Brown <bruinbrown@gmail.com>
2014-03-16 14:02:12 +0000

    Completed type provider

--- WordTypeProvider/WordTypeProvider.fs
+++ WordTypeProvider/WordTypeProvider.fs
@@ -13,8 +13,21 @@ type WordTypeProvider(cfg:TypeProviderConfig) as this =
     let baseType = Some typeof<obj>
     let ns = "Word.TypeProvider"
     let asm = Assembly.GetExecutingAssembly()
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    let rec createTypes wordSoFar =
+        alphabet
+        |> Seq.map (fun t -> let newChar = t |> string
+                             let newWord = wordSoFar + newChar
+                             let ty = ProvidedTypeDefinition(newChar, None)
+                             ty.AddMembersDelayed(fun () -> createTypes newWord)
+                             let wordProp = ProvidedProperty("Word", typeof<string>, IsStatic=true, GetterCode = fun args -> <@@ newWord @@>)
+                             ty.AddMember(wordProp)
+                             ty)
+        |> Seq.toList
 
     let rootType = ProvidedTypeDefinition(asm, ns, "StartHere", None)
+    do rootType.AddMembersDelayed(fun () -> createTypes "")
     do this.AddNamespace(ns, [rootType])
 
 [<assembly:TypeProviderAssembly>]
