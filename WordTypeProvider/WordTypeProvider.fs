9b5f9d1df0ce8d829dc3ff1137e794d5bf65576d
Anthony Brown <bruinbrown@gmail.com>
2014-03-16 13:45:39 +0000

    Initial skeleton commit

---
+++ WordTypeProvider/WordTypeProvider.fs
@@ -0,0 +1,21 @@
+﻿namespace WordTypeProvider
+
+open Microsoft.FSharp.Core.CompilerServices
+open Microsoft.FSharp.Quotations
+open ProviderImplementation.ProvidedTypes
+open System
+open System.Reflection
+
+[<TypeProvider>]
+type WordTypeProvider(cfg:TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces()
+
+    let baseType = Some typeof<obj>
+    let ns = "Word.TypeProvider"
+    let asm = Assembly.GetExecutingAssembly()
+
+    let rootType = ProvidedTypeDefinition(asm, ns, "StartHere", None)
+    do this.AddNamespace(ns, [rootType])
+
+[<assembly:TypeProviderAssembly>]
+do ()
